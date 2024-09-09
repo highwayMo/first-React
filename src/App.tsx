102a4db1b53c250531f5c8cd9bbd317e1bbaddc0
highwayMo <1464723055@qq.com>
2024-09-09 22:37:31 +0900

    1 step

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import { Button } from '@chakra-ui/react'
+import { Box, Button, ChakraProvider, Checkbox, Flex, Input, Text } from '@chakra-ui/react'
 import './App.css'
-import { Title } from './Title'
 
 function App() {
-  return (
-      <div>
-        <Title />
-        <Title />
-        <Title />
-        <Button background='red' color='balck'>Hello</Button>
-      </div>
-  )
+ const testData = [
+   // 支出と入金のデータを作成
+   {
+     id: 1,
+     title: 'お金を払う',
+     isIncome: false,
+     amount: 1000,
+   },
+   {
+     id: 2,
+     title: 'お金をもらう',
+     isIncome: true,
+     amount: 1000,
+   },
+ ]
+ return (
+   <ChakraProvider>
+     <div>
+       <Text fontSize="2xl">家計簿アプリ</Text>
+       <Box mb="8px">
+         <Input placeholder='支出を入力' mb="4px" />
+         <Flex align="center" justifyContent="space-between">
+           <Checkbox w="100px">入金</Checkbox>
+           <Button colorScheme="teal">追加</Button>
+         </Flex>
+       </Box>
+       <div>
+         {testData.map((data) => (
+           <div key={data.id}>
+             <Flex align="center" justifyContent="space-between">
+               <Text>{data.title}</Text>
+               <Text>{data.isIncome ? "+" : "-"}{data.amount}</Text>
+             </Flex>
+           </div>
+         ))}
+       </div>
+     </div>
+   </ChakraProvider>
+ )
 }
 
 export default App
